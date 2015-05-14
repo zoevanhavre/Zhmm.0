6e5d9b1bd83d562a04376ee936c920b69cd5c2a3
Zoe van Havre <zoevanhavre@gmail.com>
2015-05-14 20:31:18 +1000

    More effort to make lyra work

--- R/ReplicateSimer2.R
+++ R/ReplicateSimer2.R
@@ -30,7 +30,7 @@ ReplicateSimer2<-function(  N, n, Kfit=10, SimID, ITERATIONS,BURN,  AMAX,  PRIOR
 		# Clean up Gibbs for lyra...
 		library(parallel)
 		Result<-mclapply(c(1:N), function(x)  {
-		gibbsHMM_PT_wDist2(YZ=SIMS[[x]],K=Kfit, densTrue=SIM_DENSITY_TRUE[[x]],  M=ITERATIONS,  alphaMAX=AMAX, type= PRIOR_TYPE, alphaMin=0.001, J=PTchain, SuppressAll="TRUE")
+		gibbsHMM_PT_wDist_LYRAfinally(YZ=SIMS[[x]],K=Kfit, densTrue=SIM_DENSITY_TRUE[[x]],  M=ITERATIONS,  alphaMAX=AMAX, type= PRIOR_TYPE, alphaMin=0.001, J=PTchain, SuppressAll="TRUE")
 
 		} , mc.cores = NumCores)
 		print(NumCores)
@@ -41,12 +41,18 @@ ReplicateSimer2<-function(  N, n, Kfit=10, SimID, ITERATIONS,BURN,  AMAX,  PRIOR
 Fin<-data.frame("Run"=rep(0,N), "MedianK0"=rep(0, N), "MeanDist"=rep(0,N))
 for(i in 1:N){
 	Fin[i,1]<-i
-	Fin[i,2]<- median(Result[[i]][["K0"]][-c(1:BURN)])
-	Fin[i,3]<-mean(Result[[i]][["f2Dist"]][-c(1:BURN)])
+	.result<-Result[[i]]
+	print(head(.result))
+	Fin[i,2]<- median(.result$K0[-c(1:BURN)])
+	Fin[i,3]<-mean(.result$f2Dist[-c(1:BURN)])
 }
-#Alive<-sapply(Result, function(x)  median( x[['K0']][-c(1:BURN)]))
-#L1norm<-sapply(Result, function(x)  mean(x[['f2Dist']][-c(1:BURN)]))
-#SmallResults<-data.frame("AliveStates"=Alive, "L1norm"=L1norm)
+
+ #Alive<-sapply(Result, function(x)  median( x[[]]$K0[-c(1:BURN)]))
+# Alive<-sapply(Result, function(x)  median( x$K0[-c(1:BURN)]))
+# L1norm<-sapply(Result, function(x)  mean(x[['f2Dist']][-c(1:BURN)]))
+# SmallResults<-data.frame("AliveStates"=Alive, "L1norm"=L1norm)
+
+
 		return(Fin)
 		}
 
